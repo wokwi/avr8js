827dcc890162e97c55e335a4c3ff3fffbd57b1d2
Uri Shaked <uri@urishaked.com>
2020-04-09 00:21:42 +0300

    style(demo): formatting, lint issue

--- demo/src/utils/editor-history.util.ts
+++ demo/src/utils/editor-history.util.ts
@@ -1,20 +1,26 @@
 const AVRJS8_EDITOR_HISTORY = 'AVRJS8_EDITOR_HISTORY';
 
 export class EditorHistoryUtil {
-  static hasLocalStorage: boolean = !!window.localStorage;
+  static hasLocalStorage = !!window.localStorage;
 
   static storeSnippet(codeSnippet: string) {
-    if (!EditorHistoryUtil.hasLocalStorage) return;
+    if (!EditorHistoryUtil.hasLocalStorage) {
+      return;
+    }
     window.localStorage.setItem(AVRJS8_EDITOR_HISTORY, codeSnippet);
   }
 
   static clearSnippet() {
-    if (!EditorHistoryUtil.hasLocalStorage) return;
+    if (!EditorHistoryUtil.hasLocalStorage) {
+      return;
+    }
     localStorage.removeItem(AVRJS8_EDITOR_HISTORY);
   }
 
   static getValue() {
-    if (!EditorHistoryUtil.hasLocalStorage) return;
+    if (!EditorHistoryUtil.hasLocalStorage) {
+      return;
+    }
     return localStorage.getItem(AVRJS8_EDITOR_HISTORY);
   }
 }
