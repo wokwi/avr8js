4ea0a4134842347425716e5b82c3872a5f3b0b8e
Elliott Kember <elliott.kember@gmail.com>
2020-07-09 20:53:15 -0700

    Emittery emitter

--- demo/src/task-scheduler.ts
+++ demo/src/task-scheduler.ts
@@ -1,7 +1,10 @@
+import Emittery from 'emittery';
+
 // Faster setTimeout(fn, 0) implementation using postMessage API
 // Based on https://dbaron.org/log/20100309-faster-timeouts
 export type IMicroTaskCallback = () => void;
 
+const emitter = new Emittery();
 export class MicroTaskScheduler {
   readonly messageName = 'zero-timeout-message';
 
@@ -11,25 +14,25 @@ export class MicroTaskScheduler {
   start() {
     if (this.stopped) {
       this.stopped = false;
-      window.addEventListener('message', this.handleMessage, true);
+      emitter.on(this.messageName, this.handleMessage);
     }
   }
 
   stop() {
     this.stopped = true;
-    window.removeEventListener('message', this.handleMessage, true);
+    this.executionQueue = [];
+    emitter.removeListener(this.messageName, this.handleMessage);
   }
 
   postTask(fn: IMicroTaskCallback) {
     if (!this.stopped) {
       this.executionQueue.push(fn);
-      window.postMessage(this.messageName, '*');
+      emitter.emit(this.messageName, '*');
     }
   }
 
-  private handleMessage = (event: MessageEvent) => {
-    if (event.data === this.messageName) {
-      event.stopPropagation();
+  private handleMessage = (event: string) => {
+    if (event === '*') {
       const executeJob = this.executionQueue.shift();
       if (executeJob !== undefined) {
         executeJob();
