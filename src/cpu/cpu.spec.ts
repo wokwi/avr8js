c34224610b49db0909b7916d0bd6f42a9f2ceeab
Uri Shaked <uri@urishaked.com>
2020-12-09 00:51:33 +0200

    test(cpu): improve test name

--- src/cpu/cpu.spec.ts
+++ src/cpu/cpu.spec.ts
@@ -1,7 +1,7 @@
 import { CPU } from './cpu';
 
 describe('cpu', () => {
-  it('should set initial value of SP to the last address of internal SRAM', () => {
+  it('should set initial value of SP to the last byte of internal SRAM', () => {
     const cpu = new CPU(new Uint16Array(1024), 0x1000);
     expect(cpu.SP).toEqual(0x10ff);
   });
