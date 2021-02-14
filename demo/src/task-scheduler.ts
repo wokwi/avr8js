0ce082e4d621bef5083f05cebaeba8b06e4eac2b
Uri Shaked <uri@urishaked.com>
2021-02-14 16:20:37 +0200

    feat(demo): web worker support
    rewrite TaskScheduler to use `MessageChannel` instead of `window.postMessage`, to make the code portable into a Web Worker.

--- demo/src/task-scheduler.ts
+++ demo/src/task-scheduler.ts
@@ -1,39 +1,34 @@
-// Faster setTimeout(fn, 0) implementation using postMessage API
-// Based on https://dbaron.org/log/20100309-faster-timeouts
 export type IMicroTaskCallback = () => void;
 
 export class MicroTaskScheduler {
-  readonly messageName = 'zero-timeout-message';
-
+  private readonly channel = new MessageChannel();
   private executionQueue: Array<IMicroTaskCallback> = [];
   private stopped = true;
 
   start() {
     if (this.stopped) {
       this.stopped = false;
-      window.addEventListener('message', this.handleMessage, true);
+      this.channel.port2.onmessage = this.handleMessage;
     }
   }
 
   stop() {
     this.stopped = true;
-    window.removeEventListener('message', this.handleMessage, true);
+    this.executionQueue.splice(0, this.executionQueue.length);
+    this.channel.port2.onmessage = null;
   }
 
   postTask(fn: IMicroTaskCallback) {
     if (!this.stopped) {
       this.executionQueue.push(fn);
-      window.postMessage(this.messageName, '*');
+      this.channel.port1.postMessage(null);
     }
   }
 
-  private handleMessage = (event: MessageEvent) => {
-    if (event.data === this.messageName) {
-      event.stopPropagation();
-      const executeJob = this.executionQueue.shift();
-      if (executeJob !== undefined) {
-        executeJob();
-      }
+  private handleMessage = () => {
+    const executeJob = this.executionQueue.shift();
+    if (executeJob !== undefined) {
+      executeJob();
     }
   };
 }
